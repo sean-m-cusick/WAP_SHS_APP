13ed40cbb907dbc91c70b4465568f63ef8cb9417
Aboubakar Hameed <74980077+aah30@users.noreply.github.com>
2022-02-01 09:44:32 +0000

    Restore

--- dashboard/global.R
+++ dashboard/global.R
@@ -7,14 +7,11 @@ library(giscoR)
 library(sf)
 library(DT)
 library(leaflet)
-library(leaflet.extras)
+#library(leaflet.extras)
 
 
 
-# Loading in Life expectancy data ----
-whisky_data <- whisky
-all_regions <- unique(whisky_data_spatial$Region)
-all_distilleries <- unique(whisky_data_spatial$Distillery)
+
 
 
 # Loading in Scotland shape file ----
@@ -22,6 +19,10 @@ whisky_data_spatial <- whisky_data %>%
   st_as_sf(coords = c("Latitude", "Longitude"), crs = 4326)
 scotland <- giscoR::gisco_get_nuts(nuts_id = 'UKM',
                                    resolution = '01')
+# Loading in Life expectancy data ----
+whisky_data <- whisky
+all_regions <- unique(whisky_data_spatial$Region)
+all_distilleries <- unique(whisky_data_spatial$Distillery)
 # scotland_shape <- st_read(here("clean_data/shape_data/pub_las.shp")) %>%
 #   st_simplify(dTolerance = 1000) %>%
 #   st_transform("+proj=longlat +datum=WGS84")
