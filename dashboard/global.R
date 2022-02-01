b3eff430cac6ead92894ba7ee651af519859f9eb
Aboubakar Hameed <74980077+aah30@users.noreply.github.com>
2022-02-01 12:12:41 +0000

    Update global.R
    Amend the path

--- dashboard/global.R
+++ dashboard/global.R
@@ -12,25 +12,25 @@ library(lubridate)
 
 
 # Loading in Life expectancy data
-life_expectancy_data <- read_csv(here("clean_data/life_expectancy_clean.csv")) %>%
+life_expectancy_data <- read_csv(here("clean_data/Temp_files/life_expectancy_clean.csv")) %>%
   mutate(local_authority = if_else(local_authority == "Na h-Eileanan Siar", "Eilean Siar", local_authority),
          value = round(value, 2)) %>%
   rename(gender = sex) %>%
   filter(age == "0 years")
 
 # Loading in Scotland shape file
-scotland_shape <- st_read(here("clean_data/shape_data/pub_las.shp")) %>%
+scotland_shape <- st_read(here("clean_data/Temp_files/shape_data/pub_las.shp")) %>%
   st_simplify(dTolerance = 1000) %>%
   st_transform("+proj=longlat +datum=WGS84")
 
 # Loading in drug data
-drug_deaths <- read_csv(here("clean_data/drug_deaths_clean.csv"))
+drug_deaths <- read_csv(here("clean_data/Temp_files/drug_deaths_clean.csv"))
 
 # Loading in alcohol data
-alcohol_deaths <- read_csv(here("clean_data/alcohol_deaths_clean.csv")) %>%
+alcohol_deaths <- read_csv(here("clean_data/Temp_files/alcohol_deaths_clean.csv")) %>%
   mutate(gender = case_when(gender == "male" ~ "Male",
                             gender == "female" ~ "Female"))
-alcohol_area <- read_csv(here("clean_data/alcohol_deaths_area.csv"))
+alcohol_area <- read_csv(here("clean_data/Temp_files/alcohol_deaths_area.csv"))
 
 #Launch App
 #shinyApp(ui = ui, server = server)
